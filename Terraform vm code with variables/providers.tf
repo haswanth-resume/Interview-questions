2826b832c1a538f680f286585a228d9e092a0a42
haswanth-k <40890509+haswanth-k@users.noreply.github.com>
2024-07-23 15:37:35 +0530

    providers.tf

---
+++ Terraform vm code with variables/providers.tf
@@ -0,0 +1,28 @@
+# provider required for windows 2022 server
+
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "3.80.0"
+    }
+  }
+}
+# provider credentials
+provider "azurerm" {
+  subscription_id = var.subscription-id
+  client_id = var.client-id
+  client_secret = var.client-secret
+  tenant_id = var.tenant-id
+  features {}
+    
+  }
+  terraform {
+  cloud {
+    organization = "Sentinal-POC"
+
+    workspaces {
+      name = "sentinal-check"
+    }
+  }
+}
